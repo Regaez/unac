8592c07b3cb81c3d0808194d6ca50da7db9ef30c
Thomas Threadgold <thomas@threadgold.nz>
2025-09-05 23:36:00 +0200

    refactor: improve button styles

--- templates/game_templ.go
+++ templates/game_templ.go
@@ -190,7 +190,7 @@ func Game(game domain.GameState) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "'s turn...</h2>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "'s turn</h2>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
